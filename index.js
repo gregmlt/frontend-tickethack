aac4b6059abd924f275559aae7f7747963394086
NaeemDy <naim.hamidi@gmail.com>
2024-04-17 14:40:57 +0200

    index.js

--- index.js
+++ index.js
@@ -1,47 +1,87 @@
+moment.locale("fr");
 
 
-// ****** fonction ******
-// display trip 
 
-moment.locale('fr');
 
-document.querySelector("#btn-search").addEventListener("click", function (){
-    const dateInput = document.querySelector("#date").value
-    const formattedTime = moment(dateInput).format("YYYY-MM-DD");
-    const getTrip = {
-        departure : document.querySelector("#departure").value,
-        arrival : document.querySelector("#arrival").value,
-        date : formattedTime,
-    };
-    console.log(formattedTime);
-    fetch("http://localhost:3000/routes/trips", {
-        method: "GET",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({getTrip})
-        })
-        .then(response => response.json())
-        .then(data => {
- 
-        for (let i = 0; i < data.length; i++) {
-            if(data[i] === getTrip.forEach(data)) {
-                ///afficher les resultats
-                document.querySelector("#select-your-train").innerHTML += `
-                <div class="train">
-                <div class="itinerary">
-                <p class="departure">${data}</p>
-                <p>></p>
-                <p class="arrival">${data}</p>
+// *************** recherche itineraire et resultat *************
+
+document.querySelector("#btn-search").addEventListener("click", function () {
+  const departure = document.querySelector("#departure").value;
+  const arrival = document.querySelector("#arrival").value;
+  const date = document.querySelector("#date").value;
+  const formattedDate = new Date(date).getTime() / 1000;
+
+  fetch(`http://localhost:3000/trips/${departure}/${arrival}/${formattedDate}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        data.allTrips.forEach((trip) => {
+          const date = moment(trip.date).format("LT");
+          document.querySelector("#display-card").innerHTML += `
+                <div id="select-your-train">
+                  <div class="train">
+                    <div class="itinerary">
+                      <p class="departure">${trip.departure}</p>
+                      <p>></p>
+                      <p class="arrival">${trip.arrival}</p>
+                    </div>
+                    <p class="departure-time">${date}</p>
+                    <p class="price">${trip.price}€</p>
+                    <input type="button" value="Book" id="btn-book" />
+                  </div> 
                 </div>
-                <p class="departure-time">${data}</p>
-                <p class="price">${data}€</p>
-                <input type="button" value="Book" id="btn-book"/>
-            </div> 
-    `
-            }}
-}).catch(error)
-
-
+                `;
+          document.querySelector(".display-list-content01").style.display =
+            "none";
+        })} else {
+      document.querySelector(".display-list-content02").style.display =
+        "flex";
+      console.log("No trips found.");
+    }
+    
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
+
+
+
+
+
+// **************** selection du trajet / btn book **************
+
+
+
+
+document.querySelector("#btn-book").addEventListener("click", function (){
+
+    fetch("http://localhost:3000/onetrip", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+    departure: data.departure,
+    arrival: "Jasmin",
+    date: "joachim.jasmin@gmail.com",
+    price: "AVeryStrongPassword!!!",
+}),
+})
+.then((response) => response.json())
+.then((data) => {
+    if (data.result === true) {
+        console.log("Youpi !")
+    } else {
+        console.log("Moins youpi...")
+    }
+})
+
+
+
+
+
+
+
+
 
 
 
